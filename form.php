5e07617a2a23289638b99b31cbe397438eed52c7
Jekobukhrashvili <bukhrashvilijeko@gmail.com>
2024-03-28 03:12:46 +0400

    Change php file

--- form.php
+++ form.php
@@ -2,10 +2,10 @@
 var_dump($_POST);
 
 if ($_POST['password'] == $_POST['confirm_password']) {
-  echo $_POST['name'] . ", Welcome!";
-  echo " Registration completed successfully.";
+  echo $_POST['name'] . ", Welcome! Registration completed successfully.";
+
 }
   else {
-    echo $_POST['name'] . ", Sorry!";
-    echo " Password and Confirm Password does not match!";
+    echo $_POST['name'] . ", Sorry! Password and Confirm Password does not match!";
+  
 }
